7a52fdb8c2cf915629e665ec6899dd4c093a6165
om pharate <ompharate31@gmail.com>
2023-12-07 19:45:59 +0530

    operations are done

---
+++ basis/operations.js
@@ -0,0 +1,17 @@
+let value = 3
+let negValue = -value;
+console.log(negValue)
+
+console.log(2**8)
+
+let str1 = "hellow"
+let str2 = "pharate"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2)
+console.log("1"+2+4) // 124
+console.log(1 + 3 + "5") // 45
+
+console.log(+true) // 1
+console.log(true+false) // 1
