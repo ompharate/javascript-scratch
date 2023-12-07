728e3a6d7dd52e4bd0c7ed4f744a136c46044bfa
om pharate <ompharate31@gmail.com>
2023-12-07 19:52:54 +0530

    comparisons are done

---
+++ basis/comparisons.js
@@ -0,0 +1,9 @@
+console.log(10<15)
+console.log(10==10)
+console.log(10===10)
+
+console.log("1"<10) // true
+console.log(undefined==0) //false
+
+console.log("10"==10); // true
+console.log("10"===10); // false
