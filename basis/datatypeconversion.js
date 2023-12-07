c651c2a5925060c2f38c3ca34bcd3bf16c03e74e
om pharate <ompharate31@gmail.com>
2023-12-07 19:18:11 +0530

    datatype conversion is done

---
+++ basis/datatypeconversion.js
@@ -0,0 +1,19 @@
+let score = "33a";
+console.log(score); 
+console.log(typeof score);
+let newNumber = Number(score)
+
+console.log(newNumber)
+console.log(typeof newNumber)
+//  true =  1
+//  false = 0 
+
+let isLoggedIn = 5;
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn)
+console.log(typeof boolIsLoggedIn)
+
+let number =6
+let stringnumberr = String(number)
+console.log(stringnumberr)
+console.log(typeof stringnumberr)
