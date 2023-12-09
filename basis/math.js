3764bbeb2ddeec5411b26ca1a67fa042233a7f7d
om pharate <ompharate31@gmail.com>
2023-12-10 00:40:15 +0530

    math and number operations is done

---
+++ basis/math.js
@@ -0,0 +1,21 @@
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(2)); // 100.00
+const num = 59;
+console.log(num.toString());
+// console.log(typeof num); number
+
+
+const num1 = 100000;
+console.log(num1.toLocaleString('en-IN')); // indian standard
+
+console.log(Math)
+console.log(Math.abs(-10)); // 10
+console.log(Math.round(50.23)) // 50
+console.log(Math.round(50.83)) // 51
+
+console.log(Math.random() * 100);
+console.log(Math.random() * 10 ); 
+console.log(Math.max(20,60,12))
+console.log(Math.min(20,60,12))
