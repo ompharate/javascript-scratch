2d6569500d93ad9da12cadc0ee6c52c47c9709dc
om pharate <ompharate31@gmail.com>
2023-12-10 00:51:49 +0530

    i am done with date and time

---
+++ basis/date.js
@@ -0,0 +1,14 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString())
+// Date is object  in javascript
+// months starts from 0 in javascript
+
+
+//  Create Date
+let CreateDate = new Date("01-14-2023");
+console.log(CreateDate.toDateString())
+console.log(CreateDate.getMonth() + 1)// Months starts form 0
