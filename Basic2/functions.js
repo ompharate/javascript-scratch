c8979ed12e234e0cc37616d265743d064751bcd0
ompharate <ompharate31@gmail.com>
2023-12-15 19:20:37 +0530

    functions is done

---
+++ Basic2/functions.js
@@ -0,0 +1,17 @@
+function addTwoNumbers(num1,num2){
+
+        console.log(num1+num2);
+
+}
+
+addTwoNumbers(10,50)
+
+
+function printNumbers(num1,num2,...num) {
+
+        console.log(num1,num2,num);
+
+}
+
+printNumbers(100,500,6000,700)
+printNumbers(1000,500,[700,500])
